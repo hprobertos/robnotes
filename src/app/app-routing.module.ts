61a5cf78d43e0f8d6411db27b6300c41fb2fe0c1
Roberto Sevilla <roberto.sevilla.varela@uabc.edu.mx>
2020-01-06 23:04:55 -0800

    init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,17 @@
-import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { PreloadAllModules, RouterModule, Routes } from "@angular/router";
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: "", redirectTo: "note", pathMatch: "full" },
+  {
+    path: "note",
+    loadChildren: () => import("./note/note.module").then(m => m.NotePageModule)
+  },
+  {
+    path: "profile",
+    loadChildren: () =>
+      import("./profile/profile.module").then(m => m.ProfilePageModule)
+  }
 ];
 
 @NgModule({
@@ -12,4 +20,4 @@ const routes: Routes = [
   ],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/note/note.page.spec.ts
+++ src/app/note/note.page.spec.ts
@@ -1,19 +1,19 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
-import { HomePage } from './home.page';
+import { NotePage } from './note.page';
 
-describe('HomePage', () => {
-  let component: HomePage;
-  let fixture: ComponentFixture<HomePage>;
+describe('NotePage', () => {
+  let component: NotePage;
+  let fixture: ComponentFixture<NotePage>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HomePage ],
+      declarations: [ NotePage ],
       imports: [IonicModule.forRoot()]
     }).compileComponents();
 
-    fixture = TestBed.createComponent(HomePage);
+    fixture = TestBed.createComponent(NotePage);
     component = fixture.componentInstance;
     fixture.detectChanges();
   }));
